ec29e0dfabdcd70fc665830cfd06d02ede901603
adarsh-2910 <adarsh.shrivastav001@gmail.com>
2022-11-21 18:05:25 +0530

    front_end_routes_done

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,31 @@
+class WelcomeController < ApplicationController
+    def index
+    end    
+
+    def blog
+    end
+    
+    def blog_single
+    end
+    
+    def cart
+    end
+    
+    def checkout
+    end
+    
+    def contact_us
+    end
+    
+    def login
+    end
+    
+    def product_details
+    end
+    
+    def shop
+    end
+    
+    def error404
+    end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '//admin', as: 'rails_admin'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "welcome/index"
+  get "welcome/blog"
+  get "welcome/blog_single"
+  get "welcome/cart"
+  get "welcome/checkout"
+  get "welcome/contact_us"
+  get "welcome/login"
+  get "welcome/product_details"
+  get "welcome/shop"
+  get "welcome/error404"
+  
 
   # Defines the root path route ("/")
   # root "users#sign_up"
